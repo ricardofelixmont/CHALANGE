5847261645e0e97f27e63599f75a891bd39c5eae
Ricardo Felix <ricardofelix_mont@outlook.com>
2019-05-03 11:27:35 -0300

    testando pipeline

--- servidor/ricardoFelixData_setup.sh
+++ servidor/ricardoFelixData_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 /home/semantix/CHALANGE/servidor/ricardoFelix/bin/gerador_ini.py
 
+# Download e do programa
+wget https://github.com/ricardofelixmont/ricardoFelix/archive/master.zip
