e47647a9c809483bd6795c289db16df6b165a5b5
Ricardo Felix <ricardofelix_mont@outlook.com>
2019-05-04 18:22:28 -0300

    finalizando as transferencias para o hdfs

--- servidor/ricardoFelix/bin/transferir_para_hdfs.sh
+++ servidor/ricardoFelix/bin/transferir_para_hdfs.sh
@@ -1,11 +1,36 @@
 #!bin/bash
 
+##### CONSOLIDAÇÃO #####
+
+### Transferencia Dolar ###
 # Descobrindo qual é o arquivo mais recente
 arquivo=$(ls -t ~/ricardoFelix/crawler_dolar |head -1)
 echo "O arquivo mais recente é: $arquivo"
 
-# hdfs dfs -put ~/ricardoFelix/crawler_dolar/$arquivo /user/ricardoFelix/input
+hdfs dfs -put ~/ricardoFelix/crawler_dolar/$arquivo /user/ricardoFelix/input
 encho "Movendo para o HDFS"
 
 echo "Guardando o arquivo mais recente em 'transferidos.zip'"
 zip ~/ricardoFelix/crawler_dolar/transferidos/transferidos.zip ~/ricardoFelix/crawler_dolar/$arquivo
+
+
+### Transferencia Crypto ###
+# Mudar para consolidados
+# armazenar todos em processados.zip
+# armazenar os transferidos em transferidos.zip
+
+# Descobrindo qual é o arquivo mais recente em ~/ricardoFelix/crawler_crypto
+arquivo_crypto=$(ls -t ~/ricardoFelix/crawler_crypto | head -1) 
+echo "O arquivo mais recente é: $arquivo_crypto"
+
+# Adicionando o arquivo crypto mais recente em processados.zip
+zip ~/ricardoFelix/crawler_crypto/processados/processados.zip ~/ricardoFelix/crawler_crypto/$arquivo_crypto
+
+# Movendo o arquivo crypto para consolidados
+mv ~/ricardoFelix/crawler_crypto/$arquivo_crypto ~/ricardoFelix/crawler_crypto/consolidados/
+
+# Movendo o arquivo para o HDFS
+hdfs dfs -put ~/ricardoFelix/crawler_crypto/consolidados/$arquivo_crypto /user/ricardoFelix/input
+
+# Adicionando o arquivo enviado para o HDFS para transferidos.zip
+zip ~/ricardoFelix/crawler_crypto/consolidados/transferidos/transferidos.zip ~/ricardoFelix/crawler_crypto/consolidados/$arquivo_crypto
