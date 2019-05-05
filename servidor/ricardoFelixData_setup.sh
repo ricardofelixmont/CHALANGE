7fd225bf52086d638bcf00c1d9fbcf87a85bae29
Ricardo Felix <ricardofelix_mont@outlook.com>
2019-05-04 23:57:54 -0300

    novos arquivos

--- servidor/ricardoFelixData_setup.sh
+++ servidor/ricardoFelixData_setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-# Criar arvore de diretórios
+# Criação de Arvore de diretórios no Linux FS
+echo "Criando arvore de diretórios..."
 mkdir -p ricardoFelix/
 mkdir -p ricardoFelix/bin
-mkdir -p ricardoFelix/crawler_crypto/processados 
+mkdir -p ricardoFelix/crawler_crypto/processados
 mkdir -p ricardoFelix/crawler_crypto/consolidados
 mkdir -p ricardoFelix/crawler_crypto/consolidados/transferidos
 mkdir -p ricardoFelix/crawler_dolar
@@ -13,4 +14,29 @@ mkdir -p ricardoFelix/processados_json
 mkdir -p ricardoFelix/processados_json/indexados
 
 
+# Criação da Arvore de diretórios no HDFS
+hdfs dfs -mkdir /user/ricardoFelix/
+hdfs dfs -mkdir /user/ricardoFelix/input/
+hdfs dfs -mkdir /user/ricardoFelix/input/processados/
+hdfs dfs -mkdir /user/ricardoFelix/output/
+hdfs dfs -mkdir /user/ricardoFelix/output/transferidos
+
+
+# Download dos crawlers
+echo "Baixando arquivos necessários..."
+wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont/CHALANGE/master/servidor/ricardoFelix/bin/crypto_crawler.py
+wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont/CHALANGE/master/servidor/ricardoFelix/bin/dolar_crawler.py
+wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont/CHALANGE/master/servidor/ricardoFelix/bin/transferir_para_hdfs.sh
+
+# Modificando caminhos dos arquivos para a máquina atual...
+caminho=$(pwd)
+
+sed -i "s|caminho_basico|${caminho}|" ~/ricardoFelix/bin/crypto_crawler.py
+sed -i "s|caminho_basico|$caminho|" ~/ricardoFelix/bin/dolar_crawler.py
+
+
+# Agendando crontab para iniciar os crawlers...
+(crontab -l ; echo "*/1 * * * * python3 ~/ricardoFelix/bin/crypto_crawler.py")| crontab -
+(crontab -l ; echo "*/1 * * * * python3 ~/ricardoFelix/bin/dolar_crawler.py")| crontab -
+(crontab -l ; echo "*/1 * * * * bash ~/ricardoFelix/bin/transferir_para_hdfs.sh")| crontab -
 
