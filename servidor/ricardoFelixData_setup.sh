f66f3c7308846e7de8fd39a9ffc2b0f21f9610a7
Ricardo Felix <ricardofelix_mont@outlook.com>
2019-05-09 00:46:11 -0300

    finalizando indexação

--- servidor/ricardoFelixData_setup.sh
+++ servidor/ricardoFelixData_setup.sh
@@ -30,6 +30,7 @@ wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont
 wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont/CHALANGE/master/servidor/ricardoFelix/bin/processamento_spark.py
 wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont/CHALANGE/master/servidor/ricardoFelix/bin/extrair_hdfs.sh
 wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont/CHALANGE/master/servidor/ricardoFelix/bin/formatando_json_elastic.py
+wget -c -P ~/ricardoFelix/bin https://raw.githubusercontent.com/ricardofelixmont/CHALANGE/master/servidor/ricardoFelix/bin/indexacao_elastic.sh
 # Modificando caminhos dos arquivos para a máquina atual...
 caminho=$(pwd)
 
